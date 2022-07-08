b7329467c99ca73a82aec8e9c810b06f959ca01f
michal.chrzeszczyk <michal.chrzeszczyk@WRO1-LDL-P10544>
2022-07-08 13:04:14 +0200

    Add project files.

---
+++ randomnumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RandomNumber // Note: actual namespace depends on the project name.
+{
+    public class RanNumGenerator
+    {
+        public static int Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0, 100);
+            Console.WriteLine(num);
+            String str = Convert.ToString(num);
+            File.WriteAllText(@"C:\Users\michal.chrzeszczyk\Desktop\Jenkins_Workspace\rnum.txt", str);
+            return num;
+        }
+    }
+}
